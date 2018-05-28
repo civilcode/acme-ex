00ca5d8956941614b990b30e698b31f5901303c1
Nicolas Charlery <nicolas@bokay.io>
2018-05-28 15:30:23 -0400

    Install Distillery

--- mix.exs
+++ mix.exs
@@ -15,6 +15,8 @@ defmodule MagasinPlatform.MixProject do
   #
   # Run "mix help deps" for examples and options.
   defp deps do
-    []
+    [
+      {:distillery, "~> 1.5", runtime: false}
+    ]
   end
 end
