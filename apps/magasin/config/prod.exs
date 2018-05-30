29dab2f8fcd1962293a84db5488d5e72e8332d17
Nicolas Charlery <nicolas@bokay.io>
2018-05-30 14:24:56 -0400

    Address deprecation warning for {:system, "DATABASE_URL"} in config
    https://hexdocs.pm/ecto/Ecto.Repo.html#module-urls
    > In case the URL needs to be dynamically configured, for example by reading a system environment variable, such can be done via the init/2 repository callback

--- apps/magasin/config/prod.exs
+++ apps/magasin/config/prod.exs
@@ -2,5 +2,4 @@ use Mix.Config
 
 config :magasin, Magasin.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: {:system, "DATABASE_URL" },
   ssl: true
--- apps/magasin/lib/magasin/repo.ex
+++ apps/magasin/lib/magasin/repo.ex
@@ -1,3 +1,7 @@
 defmodule Magasin.Repo do
   use Ecto.Repo, otp_app: :magasin
+
+  def init(_type, config) do
+    {:ok, Keyword.put(config, :url, System.get_env("DATABASE_URL"))}
+  end
 end
