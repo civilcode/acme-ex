5bf5f8197b47ac214b3310b96fcb5f2af77a98f9
Hugo Frappier <hugo@cekoya.com>
2019-01-12 18:38:58 -0500

    fix: Format code

--- apps/magasin/tag_wip.ex
+++ apps/magasin/tag_wip.ex
@@ -6,12 +6,12 @@ defmodule CivilCredo.Check.Design.TagWip do
   @explanation [
     check: @moduledoc,
     params: [
-      regex: "Any tests tagged as WIP",
+      regex: "Any tests tagged as WIP"
     ]
   ]
   @default_params [
     # credo:disable-for-next-line
-    regex: ~r/@(tag|moduletag) :wip/,
+    regex: ~r/@(tag|moduletag) :wip/
   ]
 
   use Credo.Check, base_priority: :high, category: :design, exit_status: 2
@@ -29,18 +29,21 @@ defmodule CivilCredo.Check.Design.TagWip do
 
   defp process_line({line_no, line}, issues, line_regex, issue_meta) do
     case Regex.run(line_regex, line) do
-      nil -> issues
+      nil ->
+        issues
+
       matches ->
-        trigger = matches |> List.last
+        trigger = matches |> List.last()
         new_issue = issue_for(issue_meta, line_no, trigger)
         [new_issue] ++ issues
     end
   end
 
   defp issue_for(issue_meta, line_no, trigger) do
-    format_issue issue_meta,
+    format_issue(issue_meta,
       message: "This test has been tagged with WIP.",
       line_no: line_no,
       trigger: trigger
+    )
   end
 end
