78f5793f4bf8494a7e212473753d3647ce52700b
Nicholas Henry <40253+nicholasjhenry@users.noreply.github.com>
2019-08-09 11:02:41 -0400

    Add error pages (#200)

--- apps/magasin_web/lib/magasin_web/views/error_view.ex
+++ apps/magasin_web/lib/magasin_web/views/error_view.ex
@@ -3,6 +3,14 @@ defmodule MagasinWeb.ErrorView do
 
   use MagasinWeb, :view
 
+  def render("404.html", _assigns) do
+    render("404_page.html", %{})
+  end
+
+  def render("500.html", _assigns) do
+    render("500_page.html", %{})
+  end
+
   def template_not_found(template, _assigns) do
     Phoenix.Controller.status_message_from_template(template)
   end
--- apps/magasin_web/test/magasin_web/views/error_view_test.exs
+++ apps/magasin_web/test/magasin_web/views/error_view_test.exs
@@ -4,10 +4,10 @@ defmodule MagasinWeb.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.html" do
-    assert render_to_string(MagasinWeb.ErrorView, "404.html", []) == "Not Found"
+    assert render_to_string(MagasinWeb.ErrorView, "404.html", []) =~ "(404)"
   end
 
   test "renders 500.html" do
-    assert render_to_string(MagasinWeb.ErrorView, "500.html", []) == "Internal Server Error"
+    assert render_to_string(MagasinWeb.ErrorView, "500.html", []) =~ "(500)"
   end
 end
